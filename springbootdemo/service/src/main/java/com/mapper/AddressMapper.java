a95b054c1a7646bab8fb783d085b3d9165e59db8
lubenweicode <1709196413@qq.com>
2026-02-11 20:09:01 +0800

    Delete springbootdemo directory

--- springbootdemo/service/src/main/java/com/mapper/AddressMapper.java
+++
@@ -1,7 +0,0 @@
-package mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import generator.domain.entity.Address;
-
-public interface AddressMapper extends BaseMapper<Address> {
-}
--- springbootdemo/service/src/main/java/com/mapper/AuthMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import generator.domain.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AuthMapper extends BaseMapper<User> {
-}
